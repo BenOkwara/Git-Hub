663cc3db73178c46b3434ee545f4694e41d33701
BenOkwara <maranathabenson@gmail.com>
2018-10-29 06:51:06 +0300

    Add form component that will hold inputs of the user

---
+++ src/app/giphy-form/giphy-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy-form',
+  templateUrl: './giphy-form.component.html',
+  styleUrls: ['./giphy-form.component.css']
+})
+export class GiphyFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
